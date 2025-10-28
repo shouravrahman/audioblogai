17e5b7d6dccea40d1cb24f83e2ed289bbb9939de
Asadur Rahman Shourav <shouravatwork@gmail.com>
2025-10-28 22:28:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5873247263-6dcaa",
+  "appId": "1:877592296234:web:dfffe2d0cd0ce945e87c53",
+  "apiKey": "AIzaSyAq6mSt0p5lEJwCZ92KywmE5pwMy9QBqIA",
+  "authDomain": "studio-5873247263-6dcaa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "877592296234"
+};
