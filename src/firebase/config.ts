879935d9bdf5223fa420929eaba349f4aff1a362
shourav <shouravrahman006@gmail.com>
2025-11-01 06:36:09 +0600

    config fixes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
-  "projectId": process.env.NEXT_PUBLIC_PROJECT_ID || "studio-5873247263-6dcaa",
-  "appId": process.env.NEXT_PUBLIC_APP_ID || "1:877592296234:web:dfffe2d0cd0ce945e87c53",
-  "apiKey": process.env.NEXT_PUBLIC_API_KEY || "AIzaSyAq6mSt0p5lEJwCZ92KywmE5pwMy9QBqIA",
-  "authDomain": process.env.NEXT_PUBLIC_AUTH_DOMAIN || "studio-5873247263-6dcaa.firebaseapp.com",
-  "messagingSenderId": "877592296234"
+	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	appId: process.env.NEXT_PUBLIC_APP_ID,
+	apiKey: process.env.NEXT_PUBLIC_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
 };
